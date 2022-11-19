6e35a8c4beae84b5dee12ceeb55cda3f904daf71
niceaji <a.ji@daum.net>
2022-11-19 22:23:12 +0900

    tailwind 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
   markdown: {
     // extendDefaultPlugins: true,
     // syntaxHighlight: "prism",
-  },
+  }
 });
